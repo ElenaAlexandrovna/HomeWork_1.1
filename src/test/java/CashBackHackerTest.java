406544b0d2f4f20f54df6d5f55d3f85aeb96cfe8
Elena Petrova <lenor1809@gmail.com>
2023-06-22 21:54:56 +0300

    Junit4 commit

--- src/test/java/CashBackHackerTest.java
+++ src/test/java/CashBackHackerTest.java
@@ -1,5 +1,5 @@
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.junit.Test;
 import ru.netology.CashbackHackService;
 public class CashBackHackerTest {
     @Test
