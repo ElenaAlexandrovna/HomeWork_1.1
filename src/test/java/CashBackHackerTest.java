94ee4a45ce682800dc21df7e814cce3d91b81358
Elena Petrova <lenor1809@gmail.com>
2023-06-22 21:48:40 +0300

    TestNG commit

---
+++ src/test/java/CashBackHackerTest.java
@@ -0,0 +1,30 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.netology.CashbackHackService;
+public class CashBackHackerTest {
+    @Test
+    public void shouldBuyHowMuchMoreTest() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+        int actual = service.remain(amount);
+        int expected = 200;
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldBuyHowMuchMoreTest1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldBuyHowMuchMoreTest2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        Assert.assertEquals(actual, expected);
+    }
+}
